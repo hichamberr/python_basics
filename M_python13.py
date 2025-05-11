8319373c41a2516b3cf522fd24905eb7d9562fdc
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:21:02 -0600

    retrieve data type

---
+++ M_python13.py
@@ -0,0 +1,20 @@
+# Integer Numbers
+print(type(10))
+print(type(-20))
+print(type(10.20))
+
+myComplexNumber = 5+6j
+print(type(myComplexNumber))
+print("Real Part is : {}".format(myComplexNumber.real))
+print("Imaginary Part is : {}".format(myComplexNumber.imag))
+
+print(100)
+print(float(100))
+print(complex(100))
+
+print(10.40)
+print(int(10.40))
+print(complex(10,10))
+
+print(10+9j)
+print(int(10+9j)) # you can not translate complex to int
