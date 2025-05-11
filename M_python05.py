0ae84be98c8ca23c1eab0b93949db423ea9471f9
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:36:55 -0600

    strings 01

---
+++ M_python05.py
@@ -0,0 +1,11 @@
+# lesson five : strings
+
+mystringOne = 'this is single quote string'
+mystringTwo = "this is double quote string"
+
+#it is normal to put single quote inside double quote and the reverse
+mystringThree = "this is triple quote 'string'"
+mystringFour = 'this is four quote "string"'
+
+
+
