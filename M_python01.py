8ed2b0fbb7e0190f41ff86c831f1e2eb974c8e36
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:35:00 -0600

    data type 01

---
+++ M_python01.py
@@ -0,0 +1,29 @@
+#lesson 01 : data type
+#type() : show you the type of data
+
+print(10)
+# to show data type we use type()
+# integer
+print(type(10))
+print(type(-100))
+
+# float
+print(type(100.5))
+
+#string
+print(type("Hello"))
+
+#list
+print(type([1,2,3,4,5])) # list
+
+#tuple
+print(type((1,2,3,7,8))) #tuple
+
+#dictionary
+print(type({"one" : 1, "two" : 2, "three" : 3})) # dict => dictionary
+
+#boolean
+print(type(2==2))
+print(type(2==3))
+
+
