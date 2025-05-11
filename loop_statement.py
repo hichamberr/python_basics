cbb5f5b9e8e80ad8b673b7216d49cd0312350254
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:47:39 -0600

    loop example

---
+++ loop_statement.py
@@ -0,0 +1,15 @@
+for i in range(1, 6):
+    if i == 4:
+        break
+    print(i)
+
+
+for i in range(1, 6):
+    if i == 4:
+        continue
+    print(i)
+
+for i in range(1, 6):
+    if i == 4:
+        pass
+    print(i)
