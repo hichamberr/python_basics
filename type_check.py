29d79e2cea3d174707601739ed67afff6b9beb9d
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:48:26 -0600

    data type check example

---
+++ type_check.py
@@ -0,0 +1,19 @@
+#how to check data variables type
+#variable declaration
+age = 38
+height = 1.86
+name = "Hicham"
+is_sdet = True
+skills = ["java", "python", "sql"]
+profile = {"name": name, "age": age, "height": height}
+
+#print data types
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_sdet))
+print(type(skills))
+print(type(profile))
+
+
