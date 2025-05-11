5b3906cb62774538f9525e5d3729a639373f655f
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:46:27 -0600

    functions

---
+++ Functions.py
@@ -0,0 +1,16 @@
+# define function
+def greet():
+    print("Hello!")
+#Function with parameter
+def greeting(name):
+    print("Hello, " + name)
+
+#Function with Return
+def add(a, b):
+    return a + b
+
+result = add(1, 2)
+print(result)
+
+
+
