fdc2655daaf422bfbfb755d987aa276ad37407ee
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:39:52 -0600

    Lists 01

---
+++ M_python15.py
@@ -0,0 +1,11 @@
+#Lists
+myAwesomeList = ["one", "two", "one", 1, 2.5 , 3, True]
+print(myAwesomeList)
+print(myAwesomeList[1])
+print(myAwesomeList[1:4])
+print(myAwesomeList[-1])
+print(myAwesomeList[-2])
+print(myAwesomeList[::2])
+print(myAwesomeList[::-1])
+myAwesomeList[1] = 2
+print(myAwesomeList)
