3e0d47f416d8544e0f356a409c8f684e72237605
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:37:45 -0600

    strings with indexing and slicing

---
+++ M_python06.py
@@ -0,0 +1,10 @@
+#indexing
+myString = "I love python"
+print(myString[0])
+print(myString[1])
+print(myString[2])
+print(myString[-1])
+
+#Slicing (access multiple sequence items)
+#[start : end]
+print(myString[2:11])
