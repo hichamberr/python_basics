948ed9bf8b9b8d9d1504d3a86f8a6606a142a01b
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:48:41 -0600

    variables example

---
+++ variables.py
@@ -0,0 +1,16 @@
+# this is a string variable (text)
+name = "Hicham"
+
+# this is an integer (whole number)
+age = 38
+
+#this is a float (decimal number)
+height = 1.86
+
+# this is a boolean
+is_tester = True
+
+print(name)
+print(age)
+print(height)
+print(is_tester)
