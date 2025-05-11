9b8e04b981a34bcf3ac62e6fdb64bb50a40187c9
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:40:26 -0600

    Lists 03

---
+++ M_python17.py
@@ -0,0 +1,28 @@
+#clear
+a = [1,2,3,4,5]
+a.clear()
+print(a)
+
+#copy
+b = [1,2,3,4,5]
+c = b.copy()
+print(b)
+
+#count
+#count how many time exist the element
+d=[1,2,3,1,4,5]
+print(d.count(1))
+
+#index
+e = [1,2,3,4,5]
+print(e.index(3))
+
+#insert
+f=[1,2,3,4,5]
+f.insert(0, "test")
+print(f)
+
+#pop
+g=[1,2,3,4,5]
+g.pop(1)
+print(g)
