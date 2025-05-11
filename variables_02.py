27f04d19fec06d4e005bdd2d2ebb334e2e730db4
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:48:51 -0600

    variables 02 example

---
+++ variables_02.py
@@ -0,0 +1,13 @@
+# define variables
+name = "Hicham"
+age = 38
+city = "Denver"
+job_title = "SDET"
+is_student = False
+
+# Step 2 : Print a sentence using those variables
+print("My name is", name)
+print("I am", age, "years old")
+print("I live in", city)
+print("I work as an", job_title)
+print("Am i a student", is_student)
