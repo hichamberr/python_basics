9112e07db7b15cc48aa16da377d49079ac635a5a
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:36:20 -0600

    variables

---
+++ M_python02.py
@@ -0,0 +1,31 @@
+# lesson 02 : variables
+myVariable = "my value"
+
+#Rules
+#we can not use dash -
+#we can not put numbers in the beginning
+#Case sensitive (Name is not  name)
+
+name = "Hicham Berr" #single word => normal
+myName = "Hicham Berr" # two words => camelCase
+my_name = "Hicham Berr" #two words snake_case
+
+print(myName)
+print(my_name)
+print(name)
+
+#you can use the same variable more than one time for different data type
+
+x = 10
+x = "Hello"
+
+print(x)
+
+# reserved keyword , words that we can not use like variables
+help("keywords")
+
+#list
+a,b,c = 1,2,3
+print(a,b,c)
+
+
