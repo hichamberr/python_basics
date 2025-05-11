2d961fd4453dd4f097bd9bf5a895db3e2e78a0ec
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:45:44 -0600

    loop 01

---
+++ break_in_loop.py
@@ -0,0 +1,18 @@
+# Break example
+for i in range(1, 6):
+    if i == 4:
+        break
+    print(i)  # Prints 1, 2, 3
+
+# Continue example
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)  # Prints 1, 2, 4, 5
+
+# loop write 1 , 2 , 3
+
+for i in range(1,4):
+    print(i)
+
+
