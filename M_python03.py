54a3925ab5e07580c07f33ea60ebac52551f73aa
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:35:52 -0600

    print lesson

---
+++ M_python03.py
@@ -0,0 +1,28 @@
+# lesson 03 : Escape sequences
+
+# Backspace \b
+
+print("Hello\bWorld") # will remove o
+
+#Escape allow new line
+print("Hello \
+I love \
+python")
+
+#escape Backslash
+print(" i love python \\")
+
+#Escape single quote
+print(' i love single quote \'test\'')
+
+#Escape double quotes
+print(" i love single quote \"test\"")
+
+#line feed : Escape new line
+print("Hello world\nHello python")
+
+# Carriage Return
+print("123456\rAbcd")
+
+#Horizontal tab : to do space
+print("Hello\tPython")
