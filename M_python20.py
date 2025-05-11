0393083d7fdaca2d0897e74f51a8ba09d01bde10
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:41:11 -0600

    Dictionary 01

---
+++ M_python20.py
@@ -0,0 +1,24 @@
+#Dictionary
+user = {
+    "name" : "hicham",
+    "age" : 38,
+    "country" : "USA"
+}
+print(user)
+print(user.keys())
+print(user.values())
+
+languages = {
+    "one" : {
+        "name" : "HTML",
+        "progress" : "80%"
+    },
+    "two" : {
+        "name" : "Css",
+        "progress" : "50%"
+    }
+}
+print(languages)
+print(len(languages))
+print(languages['one'])
+print(languages['two'])
