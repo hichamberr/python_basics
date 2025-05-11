bf30e70b5a7993cd72e8c04de12c0ba1e115019d
hichamberr <hichamberr480@gmail.com>
2025-05-11 11:39:35 -0600

    calculator

---
+++ M_python14.py
@@ -0,0 +1,26 @@
+#Addition
+print(10+30)
+print(1+4)
+print(2+4.5)
+
+#subtraction
+print(10-5)
+print(12-5.6)
+
+#multiplication
+print(10*2)
+print(5*100)
+
+#division
+print(100/10)
+
+#modulus
+print(8%2)
+print(9%2)
+
+#Exponent
+print(2**5)
+
+#floor division
+print(100//20)
+print(120%20)
